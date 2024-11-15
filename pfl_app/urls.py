662681fbd48486e3c6279b810a9caa3c6c834921
DHA-VUONGHOANG <hoangnhuvuong300103@gmail.com>
2024-11-15 20:29:23 +0700

    transform

--- pfl_app/urls.py
+++ pfl_app/urls.py
@@ -6,15 +6,10 @@ from django.urls import path, include
 app_name = 'pfl_app'
 
 urlpatterns = [
-    # Define the results URL pattern first
     path('results/', views.results_view, name='results_view'),
 
-    # Define the generic member/func pattern
     path('<member>/<func>/', views.member, name='member'),
-
-    # Other paths for preferences or additional functionality
-    # path('save_preferences/', views.save_preferences, name='save_preferences'),
-    
+   
     path('save_preferences_and_run_script/', views.save_preferences_and_run_script, name='save_preferences_and_run_script'),
 
     
